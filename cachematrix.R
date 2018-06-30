c7aac4980e45ef41e441f27251af9fe4118e0b76
Mostafa Sakr <Mostafa_Sakr@mentor.com>
2018-06-30 15:34:56 +0100

    Implement code for cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a new type of cached matrix. This new type is actually a list of 
+## some functions taking advantage of lexical scoping
+## The new type declares 4 methods:
+## get() to retrieve the matrix
+## set(mat) to set the matrix
+## getInverse() to retrieve the cached inverse, this can be NULL
+## setInverse(inverse) to cache the inverse matrix
 
-## Write a short comment describing this function
+## To use this cache matrix use cacheSolve which calculates the inverse of the cache matrix
+## if the inverse was calculated before and if the matrix has not changed, then the cacheSolve function
+## will return the cached inverse otherwise it will calculate and store it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    inv <- NULL
+    set <- function(mat) {
+        x <<- mat
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    
+    list(set = set, 
+         get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inverse <- x$getInverse()
+    
+    if(!is.null(inverse)) {
+        return(inverse)
+    }
+    
+    mat <- x$get()
+    inverse <- solve(mat, ... = ...)
+    x$setInverse(inverse)
+    inverse
 }
